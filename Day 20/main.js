999a020a0ac5dc3d0ac9fc3e2034872d93ce9592
Chrisgarlick <cgarlick94@gmail.com>
2021-07-30 14:54:44 +0100

    Added Day 20

---
+++ Day 20/main.js
@@ -0,0 +1,15 @@
+function showPage(page) {
+    document.querySelectorAll('div').forEach(div => {
+        div.style.display = 'none';
+    });
+
+    document.querySelector(`#${page}`).style.display = 'flex';
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('button').forEach(button => {
+        button.onclick = function() {
+            showPage(this.dataset.page);
+        }
+    })
+});
